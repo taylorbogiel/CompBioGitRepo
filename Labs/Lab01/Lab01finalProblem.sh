c79df5e1c484ba687482f71d2d74b1f987a4df3f
TaylorBogiel <tabo9466@colorado.edu>
2019-02-01 14:56:07 -0700

    removed dollar signs

--- Labs/Lab01/Lab01finalProblem.sh
+++ Labs/Lab01/Lab01finalProblem.sh
@@ -1,2 +1,2 @@
-$ cut -f 2-4 -d , PredPreyData.csv | head -n 1 >NewPredPreyData.csv
-$ cut -f 2-4 -d , PredPreyData.csv | tail >> NewPredPreyData.csv
+cut -f 2-4 -d , PredPreyData.csv | head -n 1 >NewPredPreyData.csv
+cut -f 2-4 -d , PredPreyData.csv | tail >> NewPredPreyData.csv
