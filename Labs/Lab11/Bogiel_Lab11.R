201ff491136624ed335d4dddf51ef00d6e4573ef
TaylorBogiel <tabo9466@colorado.edu>
2019-04-12 12:13:59 -0600

    forgot to push this on Friday

--- Labs/Lab11/Bogiel_Lab11.R
+++ Labs/Lab11/Bogiel_Lab11.R
@@ -1,6 +1,7 @@
 #Lab 11
 library("tidyverse")
 library("dplyr")
+library("ggplot2")
 #Part 1 - Download DryadData and turn it into a csv, then import data
 WoodData <- read.csv("GlobalWoodDensityDatabase.csv", stringsAsFactors = F)
 
@@ -14,4 +15,22 @@ NewWoodData <- WoodData[-c(12150),]
   #each species listed only once
   #the "Family" and "Binomial" information for each species
   #the mean of the "Density" measurements for each species
-FBM <- summarise(group_by("NewWoodData", "Family", "Binomial", "Wood.density"))
+FBM <- summarise(group_by(NewWoodData, Family, Binomial), meanDiff = mean(Wood.density))
+  #this has 8412 rows
+
+#step 6a - make a new data frame that has the average density for each Family
+FM <- summarise(group_by(NewWoodData, Family), meanDiff = mean(Wood.density))
+  #this has 191 rows
+
+#step 6b - sort the resultof 6a by MeanDensity
+MF <- arrange(FM, meanDiff)
+
+#step 6c - use results from 6b to answer:
+  #What are the 8 families with the highest average densities?
+  #what are the 8 families with the lowest average densities?
+tail(MF, n = 8)
+#Hypericacae, Zygophyllaceae, Irvingiaceae, Sarcolaenacae, Surianaceae, Linaceae, Bonnetiaceae, Quiinaceae  
+head(MF, n = 8)
+#Caricaceae, Paulowniaceae, Moringaceae, Hernandiaceae, Bixaceae, Muntingiaceae, Datiscaceae, Tetramelaceae
+
+#Part 3 - 
