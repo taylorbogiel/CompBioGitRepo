1bf08f5656ac553596a1d88286c6c5daf6f1779b
TaylorBogiel <tabo9466@colorado.edu>
2019-04-18 13:17:38 -0600

    adding project files

---
+++ Independent_Project/Independant_Project_Coding.R
@@ -0,0 +1,7 @@
+#Independant Project
+setwd("C:/Users/user/Desktop/School/Spring_2019/Computational_Biology/compBioSandbox/newGitRepo/Independent_Project")
+
+DataSet1 <- read.csv("01_Dryad_DJ12_groups (1).csv")
+DataSet2 <- read.csv("02_Dryad_DJ12_homerange.csv")
+DataSet3 <- read.csv("03_Dryad_DJ12_id_cov.csv")
+DataSet4 <- read.csv("04_Dryad_DJ12_relatedness_Q.csv")
